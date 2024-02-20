6e954ef61a71a72691ca6ef29f16ad49df6963f3
jacekhebda85 <151734131+jacekhebda85@users.noreply.github.com>
2024-02-20 22:44:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+indices = ['ads', 'keywords', 'positions', 'rating', 'suggestions']
+
+for index in indices:
+
+    output = []
+
+    with open(f'ua_{index}.json', 'r') as file:
+        lines= file.readlines()
+        for line in lines:
+            id=line.split('"')[3]
+            output.append('{"index":{"_id":"'+id+'"}}\n')
+            output.append(line.split('_source": ')[1].replace("}}", "}"))
+    with open(f'serpstat_ua_{index}.json', 'w', encoding='utf-8') as file: 
+        file.writelines(output)
